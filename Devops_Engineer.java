90ca11dd1976b241b5d10584d6ef6736ede78b36
VinitRN <01fe22bca044@kletech.ac.in>
2024-04-01 11:57:42 +0530

    program for DevopsEnginner

---
+++ Devops_Engineer.java
@@ -0,0 +1,28 @@
+
+class DevOps_Engineer extends Employee
+{
+    float bonus;
+    float total_salary;
+
+    public DevOps_Engineer(String name, int id, float salary) {
+        super(name, id, salary);
+    }
+    void compute_salary(double bonus)
+    {
+        bonus = (super.salary * bonus * 0.01);
+        total_salary = (float) (super.salary + bonus);
+    }
+    void display_details()
+    {
+        System.out.println("Employee details: ");
+        System.out.println("Nmae: "+super.name);
+        System.out.println("Id: "+super.id);
+        System.out.println("Salary: "+super.salary);
+        System.out.println("Total Salary: "+total_salary);
+    }
+    public static void main(String[] args) {
+        DevOps_Engineer de = new DevOps_Engineer("Krutika", 052, 50000 );
+        de.compute_salary(10);
+        de.display_details();
+    }
+}
